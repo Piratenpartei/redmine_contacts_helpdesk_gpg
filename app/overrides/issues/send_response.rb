2046d37cf1bfc116ac68f7b726cdc5add97d48bb
dpausp <dpausp@posteo.de>
2019-06-09 16:03:37 +0000

    move gpg send options to the top of the mail / comment area
    * Helpdesk 4.1.0 changed the mail form to show the mail switch above the
    comment box. Our GPG options should be close to it.

---
+++ app/overrides/issues/send_response.rb
@@ -0,0 +1,5 @@
+Deface::Override.new virtual_path: 'issues/_send_response',
+                     name: 'add-gpg-send-options',
+                     insert_bottom: '.helpdesk-ticket-to',
+                     original: '9d287bc4814d61277e48d22cbe50fcb47d1d7e52',
+                     partial: 'issues/gpg_send_response'
