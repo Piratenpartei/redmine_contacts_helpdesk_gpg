bb7fddc44831f2c825a0b3640ac461d8c22e5e65
dpausp <dpausp@posteo.de>
2019-06-10 11:45:43 +0000

    some js cleanups
    * use let
    * more consistent naming

--- assets/javascripts/gpg_send_response.js
+++ assets/javascripts/gpg_send_response.js
@@ -1,35 +1,36 @@
-function showGPGHints() {
-  var enabled = isGPGEncryptionEnabled();
+function showAllGpgKeyIndicators() {
+  let enabled = isGpgEncryptionEnabled();
   $([$('#helpdesk_to'), $('#helpdesk_cc'), $('#helpdesk_bcc')]).each(function(index, select2) {
-    showTagsGPGHint(select2, enabled);
+    showGpgKeyIndicatorsForSelectField(select2, enabled);
   });
 }
 
-function showTagsGPGHint(select2, enabled) {
-  // check existing 'tagged' adresses
+function showGpgKeyIndicatorsForSelectField(select2, enabled) {
   if (enabled) {
     $(select2).find(":selected").each(function(index, option) {
-      checkGPGKeyFor(select2, $(option));
+      checkGpgKeyForOption(select2, $(option));
     });
   } else {
     $(select2).next().find('.gpg').removeClass('recipient-check-key recipient-has-key recipient-has-no-key');
   }
 }
 
-function checkGPGKeyFor(select2, option) {
-  var email = option.data('data').id;
+function checkGpgKeyForOption(select2, option) {
+  let email = option.data('data').id;
 
+  // The select2 choice element can change in the meantime.
+  // Saving the reference for later may not work, we must find the current one when we want to manipulate it.
   function findChoice() {
     return select2.next().find('.select2-selection__choice:contains("' + email + '")');
   }
 
+  findChoice().addClass('recipient-check-key').addClass('gpg');
+
   function setHint(data) {
     // Sets css style of an element according to result of (previous) check.
-    var cls = ('true' == data) ? 'recipient-has-key' : 'recipient-has-no-key';
+    let cls = ('true' == data) ? 'recipient-has-key' : 'recipient-has-no-key';
     findChoice().addClass(cls).addClass('gpg').removeClass('recipient-check-key');
   };
-
-  findChoice().addClass('recipient-check-key').addClass('gpg');
 
   // ajax call for checking availability of a key.
   $.get( '../gpgkeys/query?id=' + email, function(data) {
@@ -38,21 +39,22 @@ function checkGPGKeyFor(select2, option) {
   });
 }
 
-function isGPGEncryptionEnabled() {
-  var _checkbox = $('#helpdesk_gpg_do_encrypt').get(0);
-  return _checkbox != null && _checkbox.checked;
+function isGpgEncryptionEnabled() {
+  let checkbox = $('#helpdesk_gpg_do_encrypt').get(0);
+  return checkbox != null && checkbox.checked;
 }
 
 function setupCheckForTags() {
   // hook onto the select2 items and add handler for adding/removing items
   $([$('#helpdesk_to'), $('#helpdesk_cc'), $('#helpdesk_bcc')]).each(function() {
+
     $(this).on('select2:select', function () {
-      showTagsGPGHint($(this), isGPGEncryptionEnabled());
+      showGpgKeyIndicatorsForSelectField($(this), isGpgEncryptionEnabled());
     });
     $(this).on('select2:unselect', function () {
-      var _cc_list = $(this);
+      let select2 = $(this);
       setTimeout(function () {
-        showTagsGPGHint(_cc_list, isGPGEncryptionEnabled());
+        showGpgKeyIndicatorsForSelectField(select2, isGpgEncryptionEnabled());
       }, 500);
     });
   });
@@ -60,17 +62,17 @@ function setupCheckForTags() {
 
 $(function() {
   // stuff from the helpdesk plugin we rely on
-  sendMailCheckbox = $('#helpdesk_is_send_mail');
-  ccFields = $('#cc_fields');
-  recipientEmail = $('#customer_to_email');
+  let sendMailCheckbox = $('#helpdesk_is_send_mail');
+  let ccFields = $('#cc_fields');
+  let recipientEmail = $('#customer_to_email');
 
   // add click handler for checking keys when encryption is enabled
-  $('#helpdesk_gpg_do_encrypt').on('change', showGPGHints);
+  $('#helpdesk_gpg_do_encrypt').on('change', showAllGpgKeyIndicators);
 
   // add handler(s) for new mail adresses which are edited inline
   setupCheckForTags();
 
-  gpgOptions = $('#helpdesk_send_response_gpg');
+  let gpgOptions = $('#helpdesk_send_response_gpg');
 
   function showGpgOptions() {
     gpgOptions.show();
@@ -87,7 +89,7 @@ $(function() {
   function toggleGpgOptions() {
     if (this.checked) {
       showGpgOptions();
-      showGPGHints();
+      showAllGpgKeyIndicators();
     } else {
       hideGpgOptions();
     }
@@ -100,6 +102,6 @@ $(function() {
   // We have to bind to the click event ourselves and show the GPG options.
   $('.icon-helpdesk-reply').on('click', function() { 
     showGpgOptions();
-    showGPGHints();
+    showAllGpgKeyIndicators();
   });
 });
